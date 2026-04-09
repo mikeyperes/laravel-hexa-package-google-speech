6e327b457f17ee29769478649175d5e0b7ac3222
mikeyperes <michael@mike-ro-tech.com>
2026-04-09 03:45:02 -0400

    Add package icon + color for settings card

--- src/Providers/GoogleSpeechServiceProvider.php
+++ src/Providers/GoogleSpeechServiceProvider.php
@@ -35,6 +35,8 @@ class GoogleSpeechServiceProvider extends ServiceProvider
         if (method_exists($registry, 'registerPackage')) {
             $registry->registerPackage('google-speech', 'hexawebsystems/laravel-hexa-package-google-speech', [
             'title' => 'Google Speech',
+            'color' => 'red',
+            'icon' => 'M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z',
             'settingsRoute' => 'settings.google-speech',
             'docsSlug' => 'google-speech',
             'instructions' => [
